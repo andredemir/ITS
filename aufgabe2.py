fa344d86e18d15253ffabd231763a3995a05f718
ada516 <jan.johannsen@haw-hamburg.de>
2024-01-04 14:05:37 +0100

    Added OTLs for Regegistration without Expiring time => TODO for next time

--- aufgabe2.py
+++ aufgabe2.py
@@ -1,3 +1,6 @@
+import time
+import uuid
+
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
 import hashlib
@@ -8,6 +11,7 @@ app.secret_key = os.urandom(24)
 
 USERS_FILE = 'users.json'
 
+
 def load_users():
     try:
         with open(USERS_FILE, 'r') as file:
@@ -15,18 +19,24 @@ def load_users():
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
+
 def save_users(users_data):
     with open(USERS_FILE, 'w') as file:
         json.dump(users_data, file, indent=2)
 
+
 # Dummy-Benutzerdaten laden
 users = load_users()
+# Mapping von OTL codes zu offenen Regestrierungs Confirmations
+pendingConfirmations = {}
+
 
 @app.route('/')
 def index():
     if 'username' in session:
         return render_template('home.html', username=session['username'])
     return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -44,6 +54,15 @@ def login():
 
     return render_template('login.html')
 
+
+def generate_otl_code():
+    return str(uuid.uuid4())
+
+
+def set_otl_starttime(code, username, passhash, salt):
+    pendingConfirmations[code] = (username, passhash, salt, time.gmtime())
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -54,12 +73,45 @@ def register():
             salt = os.urandom(16).hex()
             hashed_password = hashlib.sha256((password_input + salt).encode()).hexdigest()
 
-            users[username] = {'password': hashed_password, 'salt': salt}
-            save_users(users)
-            
-            return redirect(url_for('login'))
+            # users[username] = {'password': hashed_password, 'salt': salt}
+            # save_users(users)
+            # open new pending Confirmation for user
+            code = generate_otl_code()
+            set_otl_starttime(code, username, hashed_password, salt)
+
+            return """
+                    <p>
+                        Um ihre Regestrierung abzuschließen muss folgender Link geklickt werden
+                    </p>
+                    <p><a href="/confirm&{code}">Regestrierung Bestätigen</a></p>
+                """.format(code=code)
 
     return render_template('register.html')
+
+
+@app.route('/confirm&<code>', methods=['GET'])
+def confirm(code):
+    if code in pendingConfirmations.keys():
+        confirmtuple = pendingConfirmations[code]
+        username = confirmtuple[0]
+        passhash = confirmtuple[1]
+        salt = confirmtuple[2]
+        users[username] = {'password': passhash, 'salt': salt}
+        save_users(users)
+
+        # TODO add Timer for expiring codes
+
+        return """
+                    <p>
+                        Die Regestrierung war erfolgreich!
+                    </p>
+                """
+    return """
+                    <p>
+                        Sie sind falsch abgebogen!
+                    </p>
+                """
+
 
 @app.route('/change_password', methods=['GET', 'POST'])
 # @basic_auth.required  # Erfordert Basic Authentication für diese Route
@@ -109,6 +161,7 @@ def change_password():
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
