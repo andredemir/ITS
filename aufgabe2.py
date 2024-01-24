8be89fd8ea8e6801ebc3d0f9e2d6f0ec5f85a780
I3ahsoxx <andre.demir@haw-hamburg.de>
2024-01-24 14:09:48 +0100

    Sessiaon

--- aufgabe2.py
+++ aufgabe2.py
@@ -5,13 +5,18 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template, request, redirect, url_for, session
 
+# Flask-App initialisieren
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+# Timeout für einmalige Links (OTL)
 OTLTIMEOUTSECONDS = 10
+
+# Dateiname für Benutzerdaten
 USERS_FILE = 'users.json'
 
 
+# Funktion zum Laden der Benutzerdaten aus einer JSON-Datei
 def load_users():
     try:
         with open(USERS_FILE, 'r') as file:
@@ -20,6 +25,7 @@ def load_users():
         return {}
 
 
+# Funktion zum Speichern der Benutzerdaten in einer JSON-Datei
 def save_users(users_data):
     with open(USERS_FILE, 'w') as file:
         json.dump(users_data, file, indent=2)
@@ -27,10 +33,12 @@ def save_users(users_data):
 
 # Dummy-Benutzerdaten laden
 users = load_users()
-# Mapping von OTL codes zu offenen Regestrierungs Confirmations
+
+# Mapping von OTL-Codes zu offenen Registrierungs-Bestätigungen
 pendingConfirmations = {}
 
 
+# Startseite
 @app.route('/')
 def index():
     if 'username' in session:
@@ -38,6 +46,7 @@ def index():
     return redirect(url_for('login'))
 
 
+# Login-Seite
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -55,14 +64,17 @@ def login():
     return render_template('login.html')
 
 
+# Funktion zum Generieren eines einmaligen Links (OTL-Codes)
 def generate_otl_code(username, timestamp):
     return hashlib.sha256((username + str(timestamp)).encode()).hexdigest()
 
 
+# Funktion zum Setzen der Startzeit für einen OTL-Code
 def set_otl_starttime(code, username, passhash, salt, timestamp):
     pendingConfirmations[code] = (username, passhash, salt, timestamp)
 
 
+# Registrierungsseite
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -73,21 +85,22 @@ def register():
             salt = os.urandom(16).hex()
             hashed_password = hashlib.sha256((password_input + salt).encode()).hexdigest()
 
-            # open new pending Confirmation for user
+            # Einen neuen offenen Bestätigungslink für den Benutzer erstellen
             timestamp = time.time()
             code = generate_otl_code(username, timestamp)
             set_otl_starttime(code, username, hashed_password, salt, timestamp)
 
             return """
                     <p>
-                        Um ihre Regestrierung abzuschließen muss folgender Link geklickt werden
+                        Um Ihre Registrierung abzuschließen, muss folgender Link geklickt werden:
                     </p>
-                    <p><a href="/confirm&code={code}">Regestrierung Bestätigen</a></p>
+                    <p><a href="/confirm&code={code}">Registrierung Bestätigen</a></p>
                 """.format(code=code)
 
     return render_template('register.html')
 
 
+# Bestätigungsseite für Registrierung
 @app.route('/confirm&code=<code>', methods=['GET'])
 def confirm(code):
     if code in pendingConfirmations.keys():
@@ -102,20 +115,20 @@ def confirm(code):
             save_users(users)
             return """
                                 <p>
-                                    Die Regestrierung war erfolgreich!
+                                    Die Registrierung war erfolgreich!
                                 </p>
                                 <p><a href="/login">Login</a></p>
                             """
     return """
                                 <p>
-                                    Der Link ist abgelaufen oder existiert nicht. Bitte regestrieren sie sich erneut.
+                                    Der Link ist abgelaufen oder existiert nicht. Bitte registrieren Sie sich erneut.
                                 </p>
-                                <p><a href="/register">Regestrierung</a></p>
+                                <p><a href="/register">Registrierung</a></p>
                             """
 
 
+# Seite für Passwortänderung
 @app.route('/change_password', methods=['GET', 'POST'])
-# @basic_auth.required  # Erfordert Basic Authentication für diese Route
 def change_password():
     if request.method == 'POST':
         username = request.form['username']
@@ -158,22 +171,25 @@ def change_password():
     return render_template('change_password.html')
 
 
+# Logout-Seite
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
 
+# Funktion zum Überprüfen und Entfernen abgelaufener OTLs
 def check_otls():
     for key in pendingConfirmations.keys():
-        # print("Im checking otls", time.gmtime().tm_sec)
         if round(time.time() - pendingConfirmations[key][3]) > OTLTIMEOUTSECONDS:
             pendingConfirmations.pop(key)
 
 
+# Hauptprogramm
 if __name__ == '__main__':
-    # scheduler to manage otls
+    # Scheduler für die Verwaltung von OTLs initialisieren und starten
     cron = BackgroundScheduler(daemon=True)
     cron.add_job(check_otls, 'interval', seconds=OTLTIMEOUTSECONDS)
     cron.start()
+    # Flask-App starten
     app.run(debug=True)
